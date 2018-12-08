4181c314f5774b2c8dbf006ce2f5af93690f410d
Caitlyn Carlisle <caitlyn.carlisle@duke.edu>
2018-12-08 17:47:19 -0500

    new

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -139,9 +139,11 @@ public class HuffProcessor {
 				out.writeBits(code.length(), Integer.parseInt(code,2));
 
 			}
+			if(input == -1) {
 			String code1 = codings[PSEUDO_EOF];
 			out.writeBits(code1.length(), Integer.parseInt(code1,2));
 			break;
+			}
 		}
 	}
 
